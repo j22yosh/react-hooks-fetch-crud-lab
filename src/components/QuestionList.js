62c947cd7703d3857ecdcd64461e4875843f5094
Joshua Welsh <j22yosh@gmail.com>
2022-07-01 21:16:11 -0400

    turning in what i have

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
+
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch('http://localhost:4000/questions')
+        .then(response => response.json())
+        .then(data => setQuestions(data));
+
+// empty dependency array means this effect will only run once (like componentDidMount in classes)
+}, []);
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>/* display QuestionItem components here after fetching */<QuestionItem /></ul>
     </section>
   );
 }
